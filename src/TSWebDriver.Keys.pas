a9779040310d99df665d6735ce6dc2a24580c0d1
Fernando Silva de Moraes <nandofsm@hotmail.com>
2024-02-22 11:21:40 -0400

    TSWebDriver keyboard keys

---
+++ src/TSWebDriver.Keys.pas
@@ -0,0 +1,75 @@
+unit TSWebDriver.Keys;
+
+interface
+
+type
+  TTSWebDriverKeys = class abstract
+    const
+      kNull = Char(57344);
+      kCancel = Char(57345);
+      kHelp = Char(57346);
+      kBackspace = Char(57347);
+      kTab = Char(57348);
+      kClear = Char(57349);
+      kReturn = Char(57350);
+      kEnter = Char(57351);
+      kShift = Char(57352);
+      kLeftShift = Char(57352);
+      kControl = Char(57353);
+      kLeftControl = Char(57353);
+      kAlt = Char(57354);
+      kLeftAlt = Char(57354);
+      kPause = Char(57355);
+      kEscape = Char(57356);
+      kSpace = Char(57357);
+      kPageUp = Char(57358);
+      kPageDown = Char(57359);
+      kEnd = Char(57360);
+      kHome = Char(57361);
+      kLeft = Char(57362);
+      kArrowLeft = Char(57362);
+      kUp = Char(57363);
+      kArrowUp = Char(57363);
+      kRight = Char(57364);
+      kArrowRight = Char(57364);
+      kDown = Char(57365);
+      kArrowDown = Char(57365);
+      kInsert = Char(57366);
+      kDelete = Char(57367);
+      kSemicolon = Char(57368);
+      kEqual = Char(57369);
+      kNumberPad0 = Char(57370);
+      kNumberPad1 = Char(57371);
+      kNumberPad2 = Char(57372);
+      kNumberPad3 = Char(57373);
+      kNumberPad4 = Char(57374);
+      kNumberPad5 = Char(57375);
+      kNumberPad6 = Char(57376);
+      kNumberPad7 = Char(57377);
+      kNumberPad8 = Char(57378);
+      kNumberPad9 = Char(57379);
+      kMultiply = Char(57380);
+      kAdd = Char(57381);
+      kSeparator = Char(57382);
+      kSubtract = Char(57383);
+      kDecimal = Char(57384);
+      kDivide = Char(57385);
+      kF1 = Char(57393);
+      kF2 = Char(57394);
+      kF3 = Char(57395);
+      kF4 = Char(57396);
+      kF5 = Char(57397);
+      kF6 = Char(57398);
+      kF7 = Char(57399);
+      kF8 = Char(57400);
+      kF9 = Char(57401);
+      kF10 = Char(57402);
+      kF11 = Char(57403);
+      kF12 = Char(57404);
+      kMeta = Char(57405);
+      kCommand = Char(57405);
+  end;
+
+implementation
+
+end.
